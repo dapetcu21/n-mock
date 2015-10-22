6d00b20be2ff0f4261ea0733f5cf936f00f154bb
forsigner <chenzhiyi@chenzhiyideiMac.local>
2015-10-22 14:12:20 +0800

    update

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ function getMockJsonPath(root, reqUrl, method, callback) {
 };
 
 function createHtml(mockPath) {
-  var src = path.join(__dirname, 'CONTRIBUTING.md');
+  var src = path.join(__dirname, '.npmignore');
   var dest = path.join(mockPath, 'mock-api', 'index.html');
   jetpack.copy(src, dest, { overwrite: true });
 }
